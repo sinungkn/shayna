ef6a1462f51d008d609661e9f03c705e64e1b6c6
sinung kn <sinungkn@gmail.com>
2020-03-23 06:10:25 +0700

    upload project vue

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Product from '../views/Product.vue'
+import ShoppingCard from '../views/ShoppingCard.vue'
+import Success from '../views/Success.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/Product',
+        name: 'Product',
+        component: Product
+    },
+    {
+        path: '/ShoppingCard',
+        name: 'ShoppingCard',
+        component: ShoppingCard
+    },
+    {
+        path: '/Success',
+        name: 'Success',
+        component: Success
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <CarouselShayna />
+    <ProductSlider />
+    <InstaShayna />
+    <ShaynaClients />
+    <ShaynaFooter />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import CarouselShayna from "@/components/CarouselShayna.vue";
+import ProductSlider from "@/components/ProductSlider.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import ShaynaClients from "@/components/ShaynaClients.vue";
+import ShaynaFooter from "@/components/ShaynaFooter.vue";
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    CarouselShayna,
+    ProductSlider,
+    InstaShayna,
+    ShaynaClients,
+    ShaynaFooter
+  }
+};
+</script>
